d433a14dde89a87660fa14fa95fff18ccb9921a2
De16liz <deligodi1628@gmail.com>
2021-06-01 10:10:35 -0500

    actualización traductor

--- v-inicio.php
+++ v-inicio.php
@@ -1,33 +1,52 @@
 
 <select name="menu">
-  <option>Español</option>
-  <option value="2">Tucano</option>
+
+  <?php
+
+    $conexion = mysqli_connect( "localhost", "root", "", "traductor3_3" );
+    $sql  = " SELECT idioma ";
+    $sql .= " FROM tb_idiomas ";
+    //$sql .= " WHERE t1.id_palabra = t2.id_palabra ";
+    //echo $sql;
+    $resultado = $conexion->query( $sql );
+
+    while( $fila = mysqli_fetch_assoc( $resultado ) )
+    {
+        $idioma = $fila[ 'idioma' ];
+        //$palabra = $fila[ 'palabra' ];
+        echo "<option value='$idioma'>$idioma</option>";
+    }
+  ?>
+  
 </select>
+
 <img src="imagenes/iconos/icono_transferir.svg" style="height: 25px;">
-<span class="icon icon-mid"><span class="icon-mail"></span></span>
+
+
 <select name="menu">
-  <option>Tucano</option>
-  <option value="2">Español</option>
+  
+  <?php
+
+    $sql  = " SELECT idioma ";
+    $sql .= " FROM tb_idiomas ";
+    $resultado = $conexion->query( $sql );
+
+    while( $fila = mysqli_fetch_assoc( $resultado ) )
+    {
+        $idioma = $fila[ 'idioma' ];
+        echo "<option value='$idioma'>$idioma</option>";
+    }
+  ?>
+
 </select>
 
 <br><br><br>
 
 
-<!--<div class="container">
-  <div class="row">
-    <div class="col-xs-12 col-md-3">
-      <div class="form-group two-fields">
-        
-        <div class="input-group">
-        <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="digite palabra"></textarea>          
-        <textarea class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Resultado"></textarea>        </div>
-      </div>
-    </div>
-  </div>
-</div>-->
+
 
 <form action="c-llamado.php" method="POST">
-    Inserte palabra<input type="text" name="palabra">
+    Inserte palabra <input type="text" name="palabra">
     <br>
     <input type="submit" value="Traducir">
 </form>
