0e1ef8856788b8a50d43964792676f7ab8b9e7c6
lizbethcaro <81368772+lizbethcaro@users.noreply.github.com>
2021-06-08 10:25:44 -0500

    códigos de ejemplos de llamados de frases para el traductor

---
+++ ejemplos_para_el_traductor/traductor.php
@@ -0,0 +1,19 @@
+<?php
+//este es un ejemplo desde funciones vistas que os ayudara para labase de tatos dfel traductor.
+
+   //Debe ser un diseño para el usuario
+   $cadena = $_GET[ 'frase' ];
+    
+   //Debe ser un diseño para el usuario
+   echo $cadena."<br>"; 
+
+   //Debe provenir de una base de datos.
+   $cadena = str_replace( "perro", "dog", $cadena );
+   $cadena = str_replace( "el", "the", $cadena );
+   $cadena = str_replace( "es", "is", $cadena );
+   $cadena = str_replace( "blanco", "white", $cadena );
+   $cadena = str_replace( "negro", "black", $cadena );
+   $cadena = str_replace( "rojo", "red", $cadena );
+   
+   //Debe ser un diseño para el usuario
+   echo $cadena;
